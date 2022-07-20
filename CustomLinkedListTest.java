7a5ae87cf1284351c044cf912f3cf221d451d4ff
Ritesh220101 <68066271+Ritesh220101@users.noreply.github.com>
2022-07-20 11:57:53 +0530

    Update CustomLinkedListTest.java

--- CustomLinkedListTest.java
+++ CustomLinkedListTest.java
@@ -3,6 +3,7 @@ package com.aurionpro.test;
 import com.aurionpro.model.CustomLinkedList;
 import com.aurionpro.model.Node;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 
 import com.aurionpro.model.*;
@@ -18,10 +19,15 @@ public class CustomLinkedListTest {
 		c.add(40);
 		c.add(50);
 
-		c.remove(20);
+		c.remove(3);
+		c.remove(1);
 		c.display();
+		System.out.println("----------------------");
+		Iterator<Integer> itr = c.iterator();
 		
-
+		while(itr.hasNext()) {
+			System.out.println(itr.next());
+		}
 	}
 
 }
